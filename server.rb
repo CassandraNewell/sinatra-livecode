933e216c6bd7fcb05bf66a0885ed1210557b6f62
Cassandra Newell <cassandra.newell@protonmail.com>
2018-12-10 16:46:53 -0500

    finished clinic

--- server.rb
+++ server.rb
@@ -6,3 +6,40 @@ require 'csv'
 require_relative 'app/restaurant.rb'
 
 set :bind,'0.0.0.0'  # bind to all interfaces, http://www.sinatrarb.com/configuration.html
+
+def make_restaurant_array
+  restaurants = []
+  CSV.foreach('restaurants.csv', headers: true) do |row|
+    restaurants << Restaurant.new(
+      row["id"],
+      row["name"],
+      row["address"],
+      row["description"],
+      row["url"],
+      row["image"]
+    )
+  end
+  restaurants
+end
+
+get '/' do
+  redirect to '/restaurants'
+end
+
+get '/restaurants' do
+  binding.pry
+  @restaurants = make_restaurant_array
+  erb :index
+end
+
+get '/restaurants/:id' do
+  restaurants = make_restaurant_array
+
+  restaurants.each do |restaurant|
+    if restaurant.id == params["id"]
+      @restaurant = restaurant
+    end
+  end
+
+  erb :show
+end
